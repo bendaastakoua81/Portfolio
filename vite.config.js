52ef44ad6c04ba3bb681b74c642998a82ad3c5c9
bendaastakoua81 <bendaastakoua77@gmail.com>
2025-12-29 15:13:02 +0100

    Fix production build: externalize emailjs and set base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  optimizeDeps: {
-    exclude: ["@emailjs/browser"],
+  build: {
+    rollupOptions: {
+      external: ["@emailjs/browser"],
+    },
   },
 });
